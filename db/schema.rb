e92258c62c513cc6c14876b04d2609307a55c3bc
Joao Helio <joaohelio@hotmail.com>
2020-10-27 21:25:06 -0300

    bookinkgs and places models updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_27_230530) do
+ActiveRecord::Schema.define(version: 2020_10_28_001710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_10_27_230530) do
     t.bigint "place_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "confirmed"
     t.index ["place_id"], name: "index_bookings_on_place_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
@@ -31,6 +32,8 @@ ActiveRecord::Schema.define(version: 2020_10_27_230530) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.text "description"
     t.index ["user_id"], name: "index_places_on_user_id"
   end
 
