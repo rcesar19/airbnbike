a9791201858098ad36a396ae29142b4c8373f338
roberto oliveira <roberto.c.o@gmail.com>
2020-10-29 19:17:39 -0300

    create_controller_places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,46 +1,37 @@
 class PlacesController < ApplicationController
-  # before_action :set_place, only: [:show, :edit, :update, :destroy]
+  before_action :set_place, only: [:show, :edit, :update, :destroy]
 
-  # GET http://localhost:3000/places
   def index
     @places = Place.all
   end
 
-  # GET http://localhost:3000/places/1
   def show
-    # @place = place.find(params[:id]) # before_action
+    @place = place.find(params[:id])
   end
 
-  # GET http://localhost:3000/places/1
   def new
     @place = Place.new
   end
 
-  # POST http://localhost:3000/places
   def create
-    # params[:place] => {"name"=>"teste", "address"=>"endereço", "rating"=>"10"}
-
     @place = Place.new(place_params)
     @place.save
 
     redirect_to place_path(@place)
   end
 
-  # GET http://localhost:3000/places/3/edit
   def edit
-    # @place = place.find(params[:id]) # before_action
+    # @place = place.find(params[:id])
   end
 
-  # PATCH http://localhost:3000/places/3
   def update
     # @place = place.find(params[:id]) # before_action
     @place.update(place_params)
     redirect_to place_path(@place)
   end
 
-  # DELETE http://localhost:3000/places/3
   def destroy
-    # @place = place.find(params[:id]) # before_action
+    # @place = place.find(params[:id])
     @place.destroy
     redirect_to places_path
   end
