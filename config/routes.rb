143c02ae2b1909403235138ee895f0b19e6edfa0
roberto oliveira <roberto.c.o@gmail.com>
2020-10-31 11:34:09 -0300

    add index in booking route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :places, only: [ :index, :create, :edit, :show, :update]
-  resources :bookings, only: [ :create, :edit, :show, :update]
+  resources :bookings, only: [ :index, :create, :edit, :show, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
