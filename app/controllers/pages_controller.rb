320498bb37fd3c9291f7e7ea6306ccefc5cd4dec
Giuliana Sturion <sturiongiuliana@gmail.com>
2020-11-05 11:19:48 -0300

    Search bar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,7 @@ class PagesController < ApplicationController
 
   skip_before_action :authenticate_user!, only: :home
 
-
   def home
-    @places = Place.all
+    @places = Place.all.sample(6)
   end
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,13 @@
 class Place < ApplicationRecord
   has_many :bookings
   has_many_attached :photos
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :address, :name ],
+    associated_against: {
+      user: [ :email ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
